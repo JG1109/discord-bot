ca4e0cb7b3fc378cbfc7fa4b9a309d5397f7acf8
Jiaqi Gu <46491224+JG1109@users.noreply.github.com>
2024-02-01 15:04:11 -0800

    Send congrats message with card
    - Enable the congrats message to also contain users newly updated level card

--- src/events/messageCreate/giveUserXP.js
+++ src/events/messageCreate/giveUserXP.js
@@ -1,6 +1,14 @@
-const { Client, Message } = require("discord.js");
+const {
+  Client,
+  Message,
+  AttachmentBuilder,
+  EmbedBuilder,
+} = require("discord.js");
 const Level = require("../../schemas/level");
 const calculateLevelXP = require("../../utils/calculateLevelXP");
+
+const { Font, RankCardBuilder, BuiltInGraphemeProvider } = require("canvacord");
+Font.loadDefault();
 
 /* function getRandomXP(min, max) {
   min = Math.ceil(min);
@@ -47,11 +55,56 @@ module.exports = async (client, message) => {
           level: level.level + 1,
         });
         // send a message congratulating the user, and telling them their new level, mentioning the user
-        message.channel.send(
+        // send the message to a specific channel
+        const channel = message.guild.channels.cache.get("1202537360856711189");
+        /* channel.send(
           `Congrats, <@${message.author.id}> has reached level ${
             level.level + 1
           }!`
-        );
+        ); */
+
+        // find the level rank of the user
+        let allLevels = await Level.find({
+          guildId: message.guild.id,
+        }).select("userId xp level -_id");
+        // sort the levels, handle the case where users have the same level
+        allLevels = allLevels.sort((a, b) => {
+          if (a.level === b.level) {
+            return b.xp - a.xp;
+          }
+          return b.level - a.level;
+        });
+        // get the current rank of the target user
+        const targetUserRank =
+          allLevels.findIndex((level) => level.userId === message.author.id) +
+          1;
+
+        // fetch the user object
+        const userObj = await message.guild.members.fetch(message.author.id);
+
+        // build a card with user's rank, level, XP, and avatar
+        const card = new RankCardBuilder()
+          .setAvatar(message.author.displayAvatarURL({ format: "png" }))
+          .setLevel(level.level + 1)
+          .setCurrentXP(0)
+          .setRequiredXP(calculateLevelXP(level.level + 1))
+          .setRank(targetUserRank)
+          .setUsername(message.author.username)
+          .setStatus(userObj.presence.status);
+
+        // send the card to the channel
+        const image = await card.build({
+          format: "png",
+        });
+        /*  const attachment = new AttachmentBuilder(image, "rank.png"); */
+
+        // send the congrats message and the card together
+        channel.send({
+          content: `Congrats, <@${message.author.id}> has reached level ${
+            level.level + 1
+          }!`,
+          files: [image],
+        });
       } else {
         await Level.updateOne(query, { xp });
       }
